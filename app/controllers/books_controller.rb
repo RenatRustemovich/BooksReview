19fdf4da87671821a7d4ddb5256583a64f985eac
Sabirov Renat <renatsabirov789@gmail.com>
2019-05-15 22:23:52 +0200

    Basic functional of Books review

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,15 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+
 	def index
+		@books = Book.all.order("created_at DESC")
 	end
+
+	def show
+		
+		end
+
 
 	def new
 		@book = Book.new
@@ -11,11 +20,37 @@ class BooksController < ApplicationController
 
 		if @book.save
 			redirect_to root_path
+		else
+			render 'new'
+		end
+
+
+	def edit  
+	end
+
+	def update
+		if @book.update(book_params)
+			redirect_to book_path "@book"
+		else
+	end
+
+	def destroy
+		@book.destroy
+		redirect_to root_path
+	end
+
+
 	end
 
 	private
+
 		def book_params
 			params.require(:book).permit(:title, :description, :author)
 		end
+
+		def find_book
+			@book = Book.find(params[:id])
+		end
+
 
 end
