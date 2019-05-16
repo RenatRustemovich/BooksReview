7c142747dbc8fbce9b7e5db9c0b84e09a8e1355c
Sabirov Renat <renatsabirov789@gmail.com>
2019-05-16 17:29:51 +0200

    Added: Log In, Log Out etc., categories, Books owners

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,11 @@ class BooksController < ApplicationController
 
 
 	def index
+		if params[:category].blank?
 		@books = Book.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+		end
 	end
 
 	def show
@@ -12,27 +16,33 @@ class BooksController < ApplicationController
 
 
 	def new
-		@book = Book.new
+		@book = current_user.books.build
+		@categories = Category.all.map { |c| [c.name, c.id]  }
 	end
 
 	def create
-		@book = Book.new(book_params)
+		@book = current_user.books.build(book_params)
+		@book.category_id = params[:category_id]
 
 		if @book.save
 			redirect_to root_path
 		else
 			render 'new'
 		end
+	end
 
 
 	def edit  
+		@categories = Category.all.map { |c| [c.name, c.id]  }
 	end
 
 	def update
 		if @book.update(book_params)
 			redirect_to book_path "@book"
 		else
+			render 'edit'
 	end
+end
 
 	def destroy
 		@book.destroy
@@ -40,12 +50,12 @@ class BooksController < ApplicationController
 	end
 
 
-	end
+
 
 	private
 
 		def book_params
-			params.require(:book).permit(:title, :description, :author)
+			params.require(:book).permit(:title, :description, :author, :category_id)
 		end
 
 		def find_book
---
+++ db/migrate/20190516125759_create_categories.rb
@@ -0,0 +1,9 @@
+nclass CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
